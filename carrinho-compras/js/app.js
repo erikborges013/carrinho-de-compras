28acd2e00b76ed5b86230c19387702677307d192
Erik Borges <erikborgesofficial@gmail.com>
2025-03-30 03:58:18 -0300

    Alteração de estudo feita no trabalho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,24 +1,25 @@
+
+let produtos = {
+    'Fone de ouvido - R$100':{nome: 'Fone de ouvido', precoUnitario: 100, precoTotal: 0},
+    'Celular - R$1400': {nome: 'Celular', precoUnitario: 1400, precoTotal: 0},
+    'Oculus VR - R$5000': {nome: 'Oculos', precoUnitario: 5000, precoTotal: 0}
+};
+let resultado = 0;
 function adicionar() {
-    let produtos = document.getElementById('produto');
-    let produtoSelecionado = produtos.value;
-//Aqui eu consegui recuperar o valor do produto selecionado
-//Agora vamos tentar recuperar o valor digitado de quantidade
+    let produtoSelecionado = document.getElementById('produto').value;
+
     let quantidade = document.getElementById('quantidade').value;
     let quantidadeConvertido = parseFloat(quantidade);
-//Aqui eu consegui recuperar o valor digitado no campo quantidade
-//Aqui embaixo iremos tentar atribuir o preço de cada produto
-    const foneDeOuvido = 'Fone de ouvido - R$100';
-    const celular = 'Celular - R$1400';
-    const oculos = 'Oculus VR - R$5000';
-    let resultado = 0;
-    if (produtoSelecionado == foneDeOuvido) {
-        resultado += 100 * quantidadeConvertido;
-        console.log(resultado);
-    } else if (produtoSelecionado == celular){
-        resultado += 1400 * quantidadeConvertido;
-        console.log(resultado);
-    } else if (produtoSelecionado == oculos){
-        resultado += 5000 * quantidadeConvertido;
-        console.log(resultado);
-    }
+
+    let produto = produtos[produtoSelecionado];
+    resultado += produto.precoUnitario * quantidadeConvertido;
+    console.log(resultado);
+
+    produtos[produtoSelecionado].precoTotal += produtos[produtoSelecionado].precoUnitario * quantidadeConvertido;
+    let listaDeProdutos = document.querySelectorAll('.texto-azul');
+    listaDeProdutos.innerHTML += produto.nome;
+
+    let valorTotal = document.getElementById('valor-total');
+    valorTotal.innerHTML = resultado.toLocaleString('pt-BR', {minimumFractionDigits:2, maximumFractionDigits:2});
+    //console.log(listaDeProdutos);
 }
